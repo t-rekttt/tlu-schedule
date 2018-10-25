7ce793a8b24899d46d8afb6ee07995f888677ba1
T-Rekt <t.to.the.rekt@gmail.com>
2018-10-26 01:37:33 +0700

    Initial config

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
+const apiRouter = require('./modules/api/');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 let PORT = process.env.PORT || 3000;
+
+app.use(session({
+  secret: 'ahihihihihi',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.success = ({ data, message }) => res.json({success: 1, data, message});
+  res.fail = ({ data, message }) => res.status(500).json({success: 0, data, message});
+
+  next();
+});
+
+app.use('/api', apiRouter);
 
 app.get('/test', (req, res) => {
   res.send('It works');
