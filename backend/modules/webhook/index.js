e64fcec9ac1a85877f82a2c2ed5990f73d1c2ea6
T-Rekt <t.to.the.rekt@gmail.com>
2018-10-31 01:39:56 +0700

    Updating method

--- backend/modules/webhook/index.js
+++ backend/modules/webhook/index.js
@@ -7,7 +7,37 @@ Router.post('*', (req, res) => {
 });
 
 Router.post('/update', (req, res) => {
-  if (!req.data) return res.fail({ message: 'Not enough data' });
+  if (!req.data || !req.data['messenger user id'] || !req.data.code) return res.json({
+    messages: [
+      {text: 'Không đủ dữ liệu!'}
+    ]
+  });
+
+  let messenger_user_id = req.data['messenger user id'];
+  let { code } = req.data;
+
+  scheduleModel.findOne({ hash: code })
+    .then(doc => {
+      if (!doc) {
+        return res.json({
+          messages: [
+            {text: 'Không tìm thấy lịch học'}
+          ]
+        });
+      }
+
+      messengerUserModel.update({ messenger_user_id }, {
+        messenger_user_id,
+        hash: code
+      }, { upsert: true })
+      .then(doc => {
+        return res.json({
+          messages: [
+            {text: 'Cập nhật thành công!'}
+          ]
+        });
+      });
+    });
 });
 
 module.exports = Router;
