28f37e6801b6bad23a2297dc9ed97ed643a73bdc
T-Rekt <t.to.the.rekt@gmail.com>
2018-10-31 13:42:56 +0700

    Remove uniqueness of hash to allow multiple account to use the same hash

--- backend/modules/db/messengerUserModel.js
+++ backend/modules/db/messengerUserModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messengerUserSchema = mongoose.Schema({
   messenger_user_id: { type: 'String', unique: true },
-  hash: { type: 'String', unique: true }
+  hash: { type: 'String' }
 });
 
 module.exports = mongoose.model('messengerusers', messengerUserSchema);
--- backend/modules/db/scheduleModel.js
+++ backend/modules/db/scheduleModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const scheduleSchema = mongoose.Schema({
   ma_sv: { type: 'String', unique: true },
   drpSemester: { type: 'String' },
-  hash: { type: 'String', unique: true },
+  hash: { type: 'String' },
   schedule: 'Mixed'
 });
 
