2cdcd0248fe0d133047c1c399dec886237f11e2e
T-Rekt <t.to.the.rekt@gmail.com>
2018-10-31 10:50:42 +0700

    Get schedule function

--- backend/modules/webhook/index.js
+++ backend/modules/webhook/index.js
@@ -1,14 +1,16 @@
 const Router = require('express').Router();
 const scheduleModel = require('../db/scheduleModel.js');
 const messengerUserModel = require('../db/messengerUserModel.js');
-const tinchi = require('tinchi-api');
+const {  } = require('tinchi-api');
 
 Router.post('/update', (req, res) => {
-  if (!req.body || !req.body['messenger user id'] || !req.body.code) return res.json({
-    messages: [
-      {text: 'Không đủ dữ liệu!'}
-    ]
-  });
+  if (!req.body || !req.body['messenger user id'] || !req.body.code) {
+    return res.json({
+      messages: [
+        {text: 'Không đủ dữ liệu!'}
+      ]
+    });
+  }
 
   let messenger_user_id = req.body['messenger user id'];
   let { code } = req.body;
@@ -38,7 +40,56 @@ Router.post('/update', (req, res) => {
 });
 
 Router.get('/tkb', (req, res) => {
+  if (!req.query || !req.query['messenger user id']) {
+    return res.json({
+      messages: [
+        {text: 'Không đủ dữ liệu!'}
+      ]
+    });
+  }
 
+  let messenger_user_id = req.body['messenger user id'];
+
+  messengerUserModel.findOne({
+    messenger_user_id
+  })
+  .then(doc => {
+    if (!doc) {
+      return res.json({
+        messages: [
+          {text: 'Vui lòng cập nhật lịch học trước khi tra cứu!'}
+        ]
+      });
+    }
+
+    let { hash } = doc;
+
+    scheduleModel.findOne({
+      hash
+    })
+    .then(doc => {
+      if (!doc) {
+        return res.json({
+          messages: [
+            {text: 'Không tìm thấy lịch học'}
+          ]
+        });
+      }
+
+      return doc.schedule;
+    })
+    .then(generateTimeline)
+    .then(groupTimelineByDay)
+    .then(timeline => {
+      console.log(timeline);
+
+      return res.json({
+        messages: [
+          {text: 'Thành công!'}
+        ]
+      });
+    })
+  });
 });
 
 Router.post('*', (req, res) => {
