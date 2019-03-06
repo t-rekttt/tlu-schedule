479963334b6920cde72414d9322df60ce90c2fa8
T-Rekt <t.to.the.rekt@gmail.com>
2019-03-06 16:07:40 +0700

    Change allow origin

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ app.listen(PORT, () => {
 
 privateApp.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:'+PORT);
+  res.setHeader('Access-Control-Allow-Origin', 'https://tkb.thao.pw');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
