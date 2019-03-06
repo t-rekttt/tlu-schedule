c5080e2f865c58cc339a0cc60a624422d8afc868
T-Rekt <t.to.the.rekt@gmail.com>
2019-03-06 15:55:02 +0700

    Private API to generate schedule table

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 require('./cron.js');
 const express = require('express');
 const app = express();
+const privateApp = express();
 const apiRouter = require('./modules/api/');
 const webhookRouter = require('./modules/webhook/');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const history = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
+const privateRouter = require('./modules/api/private.js');
 
 let PORT = process.env.PORT || 3000;
 
@@ -48,6 +50,8 @@ app.use('/api', apiRouter);
 
 app.use('/webhook', webhookRouter);
 
+app.use('/scheduletable', express.static(__dirname + '/scheduletable'));
+
 app.use(history());
 
 app.use('/', express.static(__dirname + '/dist'));
@@ -58,4 +62,35 @@ app.get('/test', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
+
+privateApp.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:'+PORT);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+privateApp.use((req, res, next) => {
+  res.success = ({ data, message }) => res.json({success: 1, data, message});
+  res.fail = ({ data, message }) => res.status(500).json({success: 0, data, message});
+
+  next();
+});
+
+privateApp.use('/api', privateRouter);
+
+privateApp.listen(process.env.PRIVATE_PORT, () => {
+  console.log(`Private API listening on port ${process.env.PRIVATE_PORT}`);
 });
--- backend/modules/api/private.js
+++ backend/modules/api/private.js
@@ -1,9 +1,31 @@
 const Router = require('express').Router();
+const scheduleModel = require('../db/scheduleModel.js');
+const userModel = require('../db/userModel.js');
+const { generateTimeline, groupTimelineByDay } = require('tinchi-api');
+var moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Ho_Chi_Minh');
+
+let filterThisWeek = (timeline) => {
+  return timeline.filter(day => day.day.isSame(moment(), 'week'));
+}
 
 Router.get('/tkb', (req, res) => {
   if (!req.query || !req.query.messenger_user_id) {
     return res.fail(null, 'Not enough data');
   }
 
-  
+  let { messenger_user_id } = req.query;
+
+  return userModel.findOne({ messenger_user_id })
+    .then(doc => {
+      let { hash } = doc;
+      return scheduleModel.findOne({ hash })
+    })
+    .then(doc => {
+      let { schedule } = doc;
+      return filterThisWeek(groupTimelineByDay(generateTimeline(schedule)));
+    })
+    .then(data => res.json(data))
 });
+
+module.exports = Router;
