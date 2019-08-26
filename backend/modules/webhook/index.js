36861803a1f9ab30b4aaf57ca915eda4d9b07c22
T-Rekt <t.to.the.rekt@gmail.com>
2019-08-26 18:29:10 +0700

    Update account linking domain

--- backend/modules/webhook/index.js
+++ backend/modules/webhook/index.js
@@ -210,7 +210,7 @@ Router.get('/tkb', (req, res) => {
                 buttons:[
                   {
                     type: 'web_url',
-                    url: 'https://tkb.vietthao.xyz/accountlink?messenger_user_id='+req.query['messenger user id'],
+                    url: 'https://tkb.thao.pw/accountlink?messenger_user_id='+req.query['messenger user id'],
                     title: 'Đăng nhập',
                     messenger_extensions: true,
                     webview_height_ratio: 'tall'
@@ -343,7 +343,7 @@ Router.get('/loginOptions', (req, res) => {
             buttons:[
               {
                 type: 'web_url',
-                url: 'https://tkb.vietthao.xyz/accountlink?messenger_user_id='+req.query['messenger user id'],
+                url: 'https://tkb.thao.pw/accountlink?messenger_user_id='+req.query['messenger user id'],
                 title: 'Đăng nhập',
                 messenger_extensions: true,
                 webview_height_ratio: 'tall'
